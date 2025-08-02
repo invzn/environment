50d2ebd0bdf73cda47f59bf07232b2507e2f327a
Chris Allen <c.allen.rosario@gmail.com>
2025-08-02 09:22:19 -0700

    Configure case sensitive pattern matching.

--- dotfiles/config/nvim/lua/config/options.lua
+++ dotfiles/config/nvim/lua/config/options.lua
@@ -6,31 +6,31 @@
 -- See: https://neovim.io/doc/user/vim_diff.html
 -- [2] Defaults - *nvim-defaults*
 
-local g = vim.g       -- Global variables
-local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
+local g = vim.g     -- Global variables
+local opt = vim.opt -- Set options (global/buffer/windows-scoped)
 
 -----------------------------------------------------------
 -- General
 -----------------------------------------------------------
-opt.mouse = 'a'                                -- Enable mouse support
-opt.clipboard = 'unnamedplus'                  -- Copy/paste to system clipboard
-opt.swapfile = false                  	       -- Don't use swapfile
-opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
+opt.mouse = 'a'                               -- Enable mouse support
+opt.clipboard = 'unnamedplus'                 -- Copy/paste to system clipboard
+opt.swapfile = false                          -- Don't use swapfile
+opt.completeopt = 'menuone,noinsert,noselect' -- Autocomplete options
 
 -----------------------------------------------------------
 -- Neovim UI
 -----------------------------------------------------------
-opt.number = true           -- Show line number
-opt.showmatch = true        -- Highlight matching parenthesis
-opt.foldmethod = 'marker'   -- Enable folding (default 'foldmarker')
+opt.number = true         -- Show line number
+opt.showmatch = true      -- Highlight matching parenthesis
+opt.foldmethod = 'marker' -- Enable folding (default 'foldmarker')
 --opt.colorcolumn = '80'      -- Line length marker at 80 columns
-opt.splitright = true       -- Vertical split to the right
-opt.splitbelow = true       -- Horizontal split to the bottom
-opt.ignorecase = true       -- Ignore case letters when search
-opt.smartcase = true        -- Ignore lowercase for the whole pattern
-opt.linebreak = false       -- Wrap on word boundary
-opt.termguicolors = true    -- Enable 24-bit RGB colors
-opt.laststatus=3            -- Set global statusline
+opt.splitright = true     -- Vertical split to the right
+opt.splitbelow = true     -- Horizontal split to the bottom
+opt.ignorecase = false    -- Ignore case letters when search
+opt.smartcase = false     -- Ignore lowercase for the whole pattern
+opt.linebreak = false     -- Wrap on word boundary
+opt.termguicolors = true  -- Enable 24-bit RGB colors
+opt.laststatus = 3        -- Set global statusline
 --opt.winborder='rounded'     -- Set the border style of floating windows
 -- Reserve a space in the gutter
 -- This will avoid an annoying layout shift in the screen
@@ -39,19 +39,19 @@ opt.signcolumn = 'yes'
 -----------------------------------------------------------
 -- Tabs, indent
 -----------------------------------------------------------
-opt.expandtab = true        -- Use spaces instead of tabs
-opt.shiftwidth = 2          -- Shift 2 spaces when tab
-opt.tabstop = 2             -- 1 tab == 2 spaces
-opt.smartindent = true      -- Autoindent new lines
+opt.expandtab = true   -- Use spaces instead of tabs
+opt.shiftwidth = 2     -- Shift 2 spaces when tab
+opt.tabstop = 2        -- 1 tab == 2 spaces
+opt.smartindent = true -- Autoindent new lines
 
 -----------------------------------------------------------
 -- Memory, CPU
 -----------------------------------------------------------
-opt.hidden = true           -- Enable background buffers
-opt.history = 100           -- Remember N lines in history
-opt.lazyredraw = true       -- Faster scrolling
-opt.synmaxcol = 240         -- Max column for syntax highlight
-opt.updatetime = 250        -- ms to wait for trigger an event
+opt.hidden = true     -- Enable background buffers
+opt.history = 100     -- Remember N lines in history
+opt.lazyredraw = true -- Faster scrolling
+opt.synmaxcol = 240   -- Max column for syntax highlight
+opt.updatetime = 250  -- ms to wait for trigger an event
 opt.maxmempattern = 5000
 
 -----------------------------------------------------------
@@ -62,33 +62,33 @@ opt.shortmess:append "sI"
 
 -- Disable builtin plugins
 local disabled_built_ins = {
-   "2html_plugin",
-   "getscript",
-   "getscriptPlugin",
-   "gzip",
-   "logipat",
-   --"netrw",
-   --"netrwPlugin",
-   --"netrwSettings",
-   --"netrwFileHandlers",
-   --"matchit",
-   "tar",
-   "tarPlugin",
-   "rrhelper",
-   "spellfile_plugin",
-   "vimball",
-   "vimballPlugin",
-   "zip",
-   "zipPlugin",
-   "tutor",
-   "rplugin",
-   "synmenu",
-   "optwin",
-   "compiler",
-   "bugreport",
-   "ftplugin",
+  "2html_plugin",
+  "getscript",
+  "getscriptPlugin",
+  "gzip",
+  "logipat",
+  --"netrw",
+  --"netrwPlugin",
+  --"netrwSettings",
+  --"netrwFileHandlers",
+  --"matchit",
+  "tar",
+  "tarPlugin",
+  "rrhelper",
+  "spellfile_plugin",
+  "vimball",
+  "vimballPlugin",
+  "zip",
+  "zipPlugin",
+  "tutor",
+  "rplugin",
+  "synmenu",
+  "optwin",
+  "compiler",
+  "bugreport",
+  "ftplugin",
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-   g["loaded_" .. plugin] = 1
+  g["loaded_" .. plugin] = 1
 end
